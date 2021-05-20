b61bed77dea65521c55a3e699380810a98eb9c4f
gitbash <kolhenagesh2017@gmail.com>
2021-05-20 16:38:34 +0530

    Added : Email validation completed

--- Email.java
+++ Email.java
@@ -1,10 +1,10 @@
-import java.util.regex.Pattern;
 import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Email {
 	public static boolean isValid(String email) {
 		
-		String emailid = "[0-9 a-z A-Z]{3}[. _ + -][0-9 a-z A-Z]{3}+[@][a-z]{10}[.][a-z]{2}([.][a-z]{2})?";
+		String emailid = "[0-9 a-z A-Z]{3}[. _ + -][0-9 a-z A-Z]{3}+[@][a-z]{10}[.][a-z]{2}[.][a-z]{2}";
 		
 		Pattern p = Pattern.compile(emailid);
 		if(email == null) {
@@ -16,7 +16,7 @@ public class Email {
 	}
 	
 	public static void main(String args[]) {
-	String email11 = "abc_xyz@bridgelabz.co";
+	String email11 = "abc+xyz@bridgelabz.co.in";
 	System.out.println(email11 +":" +isValid(email11));
 	}			
 }
